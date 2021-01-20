b3a061c99c86e4198a17007789a2aa12d821759f
JaceB41 <jace.bennet41@gmail.com>
2021-01-20 12:17:08 +0200

    Adding debug.c and debug.h so we can see what is happening inside the chunks

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include "common.h"
 #include "chunk.h"
+#include "debug.h"
 
 int main(int argc, const char* argv[]) {
 	Chunk chunk;
 	initChunk(&chunk);
+	disassembleChunk(&chunk, "test chunk");
 	writeChunk(&chunk, OP_RETURN);
 	freeChunk(&chunk);
 
